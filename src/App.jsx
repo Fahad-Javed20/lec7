791331d84e55ce75bd336d8620874c082edff555
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-09-21 13:16:20 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import Profile_Card from "./components/Profile_Card"
-
+import Profile_Card from "./components/Profile_Card";
 
 function App() {
+  const user1 = {
+    name: "Fahad Bin Javed",
+    title: "Website Developer",
+    picture:
+      "https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  };
   
+  const user2 = {
+    name: "Usama Bin Javed",
+    title: "App Developer",
+    picture:
+      "https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  };
+
+  const user3 ={
+    name: "Shehroz Bin Javed",
+    title: "Software Engineer",
+    picture:
+      "https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+  }
+
 
   return (
     <>
-    <div className="flex justify-center items-center h-screen">
-        <Profile_Card name = "Fahad Bin Javed" title = "web Developer" picture = "https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" /> 
-    </div>
-      
+      <div className=" md:flex-row flex flex-col justify-center items-center h-screen gap-2">
+        <Profile_Card user={ user1 } />
+        <Profile_Card user={ user2 } />
+        <Profile_Card user={ user3 } />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Profile_Card.jsx
+++ src/components/Profile_Card.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React from "react";
 
-export default function Profile_Card({picture,name,title}) {
+export default function Profile_Card({ user }) {
+
+
+
   return (
-    
-    <div className='bg-white shadow-xl/30 w-180 h-80 flex items-center justify-center rounded-4xl'>
-        <div>
-            <img  className=' h-52 w-52 rounded-full mr-12' src={picture} alt="" />
-        </div>
-        
-        <div className='mb-8 flex flex-col'>
-            <h1 className='text-3xl font-bold mb-4'>{name}</h1>
-            <h2 className='text-2xl'>{title.toUpperCase()}</h2>
-        </div>
+    <div className="bg-white shadow-xl/30 w-160 h-80 flex items-center justify-center rounded-4xl">
+      <div>
+        <img className=" h-52 w-52 rounded-full mr-12" src={user.picture} alt="" />
+      </div>
+
+      <div className="mb-8 flex flex-col">
+        <h1 className="text-3xl font-bold mb-4">{user.name}</h1>
+        <h2 className="text-2xl">
+          {user.title?.toUpperCase() ?? "Software Engineer"}
+        </h2>
+      </div>
     </div>
-  )
+  );
 }
