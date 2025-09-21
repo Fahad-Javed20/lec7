9084035f24c716fc52ec25f7feb2d5f159fc7e9d
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-09-21 11:48:23 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
 
   return (
     <>
-    <div className="flex justify-center items-center bg-red-400">
-        <Profile_Card /> 
+    <div className="flex justify-center items-center h-screen">
+        <Profile_Card name = "Fahad Bin Javed" title = "web Developer" picture = "https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" /> 
     </div>
       
     </>
--- src/components/Profile_Card.jsx
+++ src/components/Profile_Card.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 
-export default function Profile_Card() {
+export default function Profile_Card({picture,name,title}) {
   return (
     
     <div className='bg-white shadow-xl/30 w-180 h-80 flex items-center justify-center rounded-4xl'>
         <div>
-            <img  className=' h-52 w-52 rounded-full mr-12' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNbkECXtEG_6-RV7CSNgNoYUGZE-JCliYm9g&s" alt="" />
+            <img  className=' h-52 w-52 rounded-full mr-12' src={picture} alt="" />
         </div>
         
-        <div className='mb-8'>
-            <h1 className='text-3xl font-bold'>Fahad Bin Javed</h1>
-            <br />
-            <h2 className='text-2xl'>Web Developer</h2>
+        <div className='mb-8 flex flex-col'>
+            <h1 className='text-3xl font-bold mb-4'>{name}</h1>
+            <h2 className='text-2xl'>{title.toUpperCase()}</h2>
         </div>
     </div>
   )
